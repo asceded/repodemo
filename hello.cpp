870f80282ca2c25e0949664cd6a42f13c7f32e7e
Daniil <xdzero@yandex.ru>
2026-03-25 12:41:48 +0000

    feat: last line

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream> 
 
 int main(){
-
+std::cout << "Last line" << std::endl; 
 std::cout << "Hello World " << std::endl;
 std::cout << "Photoshop" << std::endl;
 std::cout << "Last line" << std::endl; 
